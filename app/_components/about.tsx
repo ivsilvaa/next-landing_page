a9e9a8eff39d2d43e03e426f1de1b146c245a27f
Ivan Silva <ivdouglas.silva@gmail.com>
2025-12-23 09:26:12 -0300

    iniciando about da pagina

---
+++ app/_components/about.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import about1Image from '../../public/images/about-1.png'
+import about2Image from '../../public/images/about-2.png'
+
+export function About() {
+    return (
+        <section className="bg-[#fdf6ec] py-16 ">
+            <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 
+            items-center">
+
+
+
+
+
+            </div>
+        </section>
+    )
+}
