ed038c469443b80947f4819cf6695aa4eb1ca892
Ivan Silva <ivdouglas.silva@gmail.com>
2025-12-22 22:19:32 -0300

    Terminando estilizacao do hero da pagina

---
+++ app/_components/hero.tsx
@@ -0,0 +1,32 @@
+import { WhatsappLogo } from '@phosphor-icons/react/dist/ssr'
+import dogImage from '../../public/images/hero-dog.webp'
+import Image from 'next/image'
+
+export function Hero() {
+    return (
+        <section className="bg-[#e84c3d] text-white relative overflow-hidden">
+            <div className='container mx-auto pt-16 pb-16 md:pb-0 px-4 relative'>
+                <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+                    <div className='space-y-6'>
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">Seu pet merece cuidado,carinho e atenção especial.</h1>
+                        <p className="lg:text-lg">Oferecemos os melhores seviços para garantir o bem-estar e a
+                            felicidade do seu amigo de quatro patas.</p>
+
+                        <a href="#" className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center
+                            justify-center w-fit gap-2">
+                            <WhatsappLogo className="w-5 h-5" />
+
+                            Contato via WhatsApp</a>
+
+                        <div className="mt-8">
+                            <p className="text-sm mb-4"><b className="bg-black text-white px-2 py-0.5 rounded-md">5%</b> de desconto na primeira compra.</p>
+                        </div>
+                    </div>
+
+
+            </div>
+        </article>
+            </div >
+        </section >
+    )
+}
