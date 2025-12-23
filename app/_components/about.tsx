078a1acd59a0162a8116bab30a1534cbac625887
Ivan Silva <ivdouglas.silva@gmail.com>
2025-12-23 09:27:47 -0300

    Adicionada as imagens o about

--- app/_components/about.tsx
+++ app/_components/about.tsx
@@ -8,7 +8,31 @@ export function About() {
             <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 
             items-center">
 
+                <div className='relative'>
+                    <div className="relative w-full h-100 rounded-3xl overflow-hidden">
+                        <Image
+                            src={about1Image}
+                            alt='Foto de um cachorro'
+                            fill
+                            quality={100}
+                            priority
+                            className='object-cover hover:scale-110 duration-300'
+                        />
+                    </div>
 
+                    <div className="absolute w-40 h-40 right-4 -bottom-8 border-4 rounded-lg 
+                    overflow-hidden border-white">
+                        <Image
+                            src={about2Image}
+                            alt='Foto de um cachorro 2'
+                            fill
+                            quality={100}
+                            priority
+
+                        />
+                    </div>
+
+                </div>
 
 
 
