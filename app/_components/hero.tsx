31c2f856386f883e956e9a1c18b1d03bb54df4b3
Ivan Silva <ivdouglas.silva@gmail.com>
2025-12-22 22:30:32 -0300

    estilizacao da imagem do cachorro finalizada

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -5,6 +5,11 @@ import Image from 'next/image'
 export function Hero() {
     return (
         <section className="bg-[#e84c3d] text-white relative overflow-hidden">
+            <div>
+                <Image src={dogImage} alt='Foto do cachorro feliz no fundo'
+                    fill sizes='100vw' priority className='object-cover opacity-60 lg:hidden' />
+                <div className='absolute inset-0 bg-black opacity-35 lg:hidden'></div>
+            </div>
             <div className='container mx-auto pt-16 pb-16 md:pb-0 px-4 relative'>
                 <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
                     <div className='space-y-6'>
@@ -25,7 +30,7 @@ export function Hero() {
 
                     <div className='hidden md:block h-full relative'>
                         <Image src={dogImage} alt="Foto do cachorrinho feliz"
-                            className='object-contain' fill sizes='(max-width: 768px) 0vw, 50vw' />
+                            className='object-contain' fill sizes='(max-width: 768px) 0vw, 50vw' quality={100} priority />
                     </div>
                 </article>
             </div>
