4d8062f57daa012954636c5ac17ee46c6395c43b
Ivan Silva <ivdouglas.silva@gmail.com>
2025-12-23 16:16:58 -0300

    terminando a estilizacao dos servicos e colocado os hovers que faltavam

--- app/_components/services.tsx
+++ app/_components/services.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import useEmblaCarousel from 'embla-carousel-react'
-import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel } from 'lucide-react'
-import { WhatsappLogo } from '@phosphor-icons/react'
+import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel, Clock } from 'lucide-react'
+import { WhatsappLogo, WhatsappLogoIcon } from '@phosphor-icons/react'
 
 const services = [
     {
@@ -58,6 +58,45 @@ export function Services() {
 
                 <div className="relative">
 
+                    <div className='overflow-hidden' ref={emblaRef}>
+                        <div className='flex'>
+                            {services.map((item, index) => (
+                                <div key={index} className='flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
+                                    <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full 
+                                    flex flex-col'>
+                                        <div className='flex-1 flex items-start justify-between'>
+
+                                            <div className='flex gap-3'>
+                                                <span className='text-3xl'>{item.icon}</span>
+                                                <div>
+                                                    <h3 className='font-bold text-xl my-2'>{item.title}</h3>
+                                                    <p className='text-gray-400 text-sm select-none'>{item.description}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                        <div className='border-t border-gray-600 pt-4 flex items-center justify-between'>
+                                            <div className='flex items-center gap-2 text-sm'>
+                                                <Clock className='w-4 h-4' />
+                                                <span>{item.duration}</span>
+                                            </div>
+
+                                            <div>
+                                                <a href="#"
+                                                    className='flex items-center justify-center gap-2 hover:scale-105 duration-300'>
+                                                    <WhatsappLogo className='w-5 h-5' />
+                                                    Entrar em contato
+                                                </a>
+                                            </div>
+                                        </div>
+
+                                    </article>
+                                </div>
+                            ))}
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
