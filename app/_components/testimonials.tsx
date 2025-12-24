00e6d69f312582e9e61dc461b0579d7739022107
Ivan Silva <ivdouglas.silva@gmail.com>
2025-12-23 22:08:00 -0300

    finalizando parte de depoimentos da pagina

--- app/_components/testimonials.tsx
+++ app/_components/testimonials.tsx
@@ -4,6 +4,8 @@ import useEmblaCarousel from 'embla-carousel-react'
 import { ChevronLeft, ChevronRight, Scissors, Syringe, CarTaxiFront, Hotel, Clock } from 'lucide-react'
 import { WhatsappLogo } from '@phosphor-icons/react'
 import tutor1 from '../../public/images/tutor1.png'
+import tutor2 from '../../public/images/tutor2.jpg'
+import tutor3 from '../../public/images/tutor3.jpg'
 import Image from 'next/image'
 
 const testimonials = [
@@ -12,20 +14,20 @@ const testimonials = [
             "Desde que comecei a levar a Luna para banho e tosa aqui, ela nunca esteve tão feliz! O atendimento é impecável, os profissionais são super cuidadosos e sempre deixam minha peluda linda e cheirosa. Recomendo de olhos fechados!",
         author: "Mariana Souza",
         role: "Tutora da Luna (Golden Retriever)",
-        image: tutor1,
+        image: tutor2,
     },
     {
         content:
             "O serviço de hotel para pets foi uma experiência incrível! Precisei viajar e fiquei tranquilo sabendo que o Thor estava sendo bem cuidado. Recebi fotos e atualizações diárias, e ele voltou para casa super feliz! Sem dúvida, o melhor petshop da região.",
-        author: "Rafael",
+        author: "Rafael Santos",
         role: "Tutor do Thor (Bulldog Francês)",
         image: tutor1,
     },
     {
         content: "Meus gatos nunca gostaram de sair de casa, mas o atendimento nesse petshop fez toda a diferença. A equipe é muito paciente e cuidadosa, e o serviço de banho especializado para felinos foi maravilhoso! Agora sei onde confiar o cuidado deles.",
-        author: "Camila fernandes",
+        author: "Camila Fernandes",
         role: "Tutora da Mel e do Max",
-        image: tutor1,
+        image: tutor3,
     },
 ]
 
@@ -58,7 +60,7 @@ export function Testimonials() {
                                 <div key={index} className='flex-[0_0_100%] min-w-0 px-3'>
                                     <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full 
                                     flex flex-col'>
-                                        <div className='flex flex-col items-center text-cneter space-y-4'>
+                                        <div className='flex flex-col items-center text-center space-y-4'>
                                             <div className='relative w-24 h-24'>
                                                 <Image
                                                     src={item.image}
@@ -69,7 +71,12 @@ export function Testimonials() {
                                                 />
                                             </div>
 
-                                            <p></p>
+                                            <p className='text-gray-200 text-center'>{item.content}</p>
+
+                                            <div className='flex flex-col items-center text-center'>
+                                                <p className='font-bold'>{item.author}</p>
+                                                <p className='text-sm text-gray-400'>{item.role}</p>
+                                            </div>
 
                                         </div>
 
